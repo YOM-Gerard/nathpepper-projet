4748bcf029d42f8e0cb0f1da5f9a399cf3fbef1b
YOM-Gerard <67868664+YOM-Gerard@users.noreply.github.com>
2026-05-24 17:45:45 +0200

    WIP: verification accolade main.css

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,17 @@
-<header class="site-header">
+<header>
     <div class="nav-container">
         <div class="logo">
             <a href="produits.php">Nathpepper</a>
         </div>
         
-        <nav class="main-nav">
-            <a href="produits.php">Boutique</a>
-            <a href="index.html#story">Notre Histoire</a>
-            
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <a href="deconnexion.php" class="btn-logout">Déconnexion</a>
-            <?php else: ?>
-                <a href="connexion.php" class="btn-login">Connexion</a>
-            <?php endif; ?>
-        </nav>
+        <a href="produits.php">Boutique</a>
+        <a href="index.html#story">Notre Histoire</a>
+        
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="deconnexion.php" class="btn-login" style="background-color: #f44336; color: #fff;">Déconnexion</a>
+        <?php else: ?>
+            <a href="connexion.php" class="btn-login">Connexion</a>
+        <?php endif; ?>
 
         <div class="cart-icon" id="btn-cart">
             🛒 <span id="cart-count">0</span>
